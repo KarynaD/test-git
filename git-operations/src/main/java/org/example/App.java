b68e79897e68b538768e8ee506b1311992a842f8
Karyna <karyna.dzenisevich@gmail.com>
2024-12-18 18:39:19 +0100

    2 commit to master

--- git-operations/src/main/java/org/example/App.java
+++ git-operations/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "First change in master" );
+        System.out.println( "Second commit to master" );
     }
 }
