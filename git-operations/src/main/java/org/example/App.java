d3cc25ca88f408bf25765cc909f78d23efabd491
Karyna <karyna.dzenisevich@gmail.com>
2025-01-07 22:24:55 +0100

    master

--- git-operations/src/main/java/org/example/App.java
+++ git-operations/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "First change in master" );
+        System.out.println( "change in master" );
     }
 }
